892da5d87040e4accd9d2bab3cb463207ee1c398
xxj <xxj@75a15a69-0cfc-47d9-a1bd-ce2941b697b2>
2012-12-20 10:42:35 +0000

    Finally tag 1.4.1 version for official release !

---
+++ tags/1.4.0/jackvst.h
@@ -0,0 +1,106 @@
+#ifndef __jack_vst_h__
+#define __jack_vst_h__
+
+#include <sys/types.h>
+#include <jack/jack.h>
+#include <jack/ringbuffer.h>
+#include <jack/session.h>
+#include <jack/midiport.h>
+#include <math.h>
+
+#include "sysex.h"
+#include "fst.h"
+
+typedef struct _JackVST JackVST;
+
+enum WantState {
+   WANT_STATE_NO     = 0,
+   WANT_STATE_RESUME = 1,
+   WANT_STATE_BYPASS = 2
+};
+
+enum WithEditor {
+   WITH_EDITOR_NO,
+   WITH_EDITOR_HIDE,
+   WITH_EDITOR_SHOW
+};
+
+enum SysExWant {
+	SYSEX_WANT_NO          = 0,
+	SYSEX_WANT_IDENT_REPLY = 1,
+	SYSEX_WANT_DUMP        = 2
+};
+
+struct _JackVST {
+    jack_client_t*  client;
+    FSTHandle*      handle;
+    FST*            fst;
+    char*           client_name;
+    char*           default_state_file;
+    short           numIns;
+    short           numOuts;
+    float**         ins;
+    float**         outs;
+    jack_port_t*    midi_inport;
+    jack_port_t*    midi_outport;
+    jack_port_t**   inports;
+    jack_port_t**   outports;
+    int             channel; /* 0 Omni, 17 None */
+    bool            bypassed;
+    enum WantState  want_state;
+    short           want_state_cc;
+    enum WithEditor with_editor;
+    double          tempo;
+    float           volume; /* where 0.0 mean silence */
+
+    int             midi_map[128];
+    bool            midi_learn;
+    short           midi_learn_CC;
+    int             midi_learn_PARAM;
+
+    /* SysEx send support */
+    pthread_mutex_t   sysex_lock;
+    pthread_cond_t    sysex_sent;
+    enum SysExWant    sysex_want;
+    bool              sysex_want_notify;
+    SysExDumpV1       sysex_dump;
+    SysExIdentReply   sysex_ident_reply;
+
+    /* SysEx receive support */
+    jack_midi_data_t* sysex_data;
+    size_t sysex_size;
+
+    /* For VST/i support */
+    bool                 want_midi_in;
+    struct VstMidiEvent* event_array;
+    struct VstEvents*    events;
+
+    /* Jack Session support */
+    char* uuid;
+    jack_session_event_t* session_event;
+
+    /* For VST midi effects & synth source (like audio to midi VSTs) support */
+    bool               want_midi_out;
+    jack_ringbuffer_t* ringbuffer;
+};
+
+bool jvst_send_sysex(JackVST* jvst, enum SysExWant);
+void jvst_bypass(JackVST* jvst, bool bypass);
+
+static inline void
+jvst_set_volume(JackVST* jvst, short volume)
+{
+	if (jvst->volume != -1) jvst->volume = powf(volume / 63.0f, 2);
+}
+
+static unsigned short
+jvst_get_volume(JackVST* jvst)
+{
+	if (jvst->volume == -1) return 0;
+
+	short ret = roundf(sqrtf(jvst->volume) * 63.0f);
+
+	return (ret < 0) ? 0 : (ret > 127) ? 127 : ret;
+}
+
+#endif /* __jack_vst_h__ */
---
+++ tags/1.4.0/sysex.h
@@ -0,0 +1,107 @@
+#ifndef __sysex_h__
+#define __sysex_h__
+
+/*
+  SysEx parser - part of FST host
+*/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define SYSEX_MAX_SIZE sizeof(SysExDumpV1)
+#define SYSEX_BEGIN 0xF0
+#define SYSEX_END 0xF7
+#define SYSEX_NON_REALTIME 0x7E
+#define SYSEX_GENERAL_INFORMATION 0x06
+#define SYSEX_IDENTITY_REQUEST 0x01
+#define SYSEX_IDENTITY_REPLY 0x02
+#define SYSEX_MYID 0x5B
+#define SYSEX_VERSION 1
+#define SYSEX_TYPE_DUMP  0
+#define SYSEX_TYPE_RQST  1
+#define SYSEX_TYPE_OFFER 2
+
+#define SYSEX_IDENT_REQUEST {SYSEX_BEGIN,SYSEX_NON_REALTIME,0x7F,SYSEX_GENERAL_INFORMATION,SYSEX_IDENTITY_REQUEST,SYSEX_END}
+typedef struct {
+	const uint8_t begin;
+	const uint8_t type;
+       	const uint8_t target_id;
+	const uint8_t gi;
+	const uint8_t ir;
+	const uint8_t end;
+} SysExIdentRqst;
+
+#define SYSEX_IDENT_REPLY {SYSEX_BEGIN,SYSEX_NON_REALTIME,0x7F,SYSEX_GENERAL_INFORMATION,SYSEX_IDENTITY_REPLY,\
+   SYSEX_MYID,{0},{0},SYSEX_VERSION,SYSEX_END}
+typedef struct {
+	const uint8_t begin;
+	const uint8_t type;
+       	const uint8_t target_id;
+	const uint8_t gi;
+	const uint8_t ir;
+	const uint8_t id;
+	const uint8_t family[2];
+	uint8_t model[2]; // Here we set sysex_uuid as [1]
+	uint8_t version[4]; // Here we set sysex_random_id for all elements
+	const uint8_t end;
+} SysExIdentReply;
+
+#define SYSEX_OFFER {SYSEX_BEGIN,SYSEX_MYID,SYSEX_VERSION,SYSEX_TYPE_OFFER,{0},0,SYSEX_END}
+typedef struct {
+	const uint8_t begin;
+	const uint8_t id;
+	const uint8_t version;
+	const uint8_t type;
+	uint8_t rnid[4]; // Copy of SysExIdentReply.version
+	uint8_t uuid; // Offered ID
+	const uint8_t end;
+} SysExIdOffer;
+
+#define SYSEX_DUMP_REQUEST {SYSEX_BEGIN,SYSEX_MYID,SYSEX_VERSION,SYSEX_TYPE_RQST,0,SYSEX_END}
+typedef struct {
+	const uint8_t begin;
+	const uint8_t id;
+	const uint8_t version;
+	const uint8_t type;
+	uint8_t uuid;
+	const uint8_t end;
+} SysExDumpRequestV1;
+
+enum SysExState {
+	SYSEX_STATE_NOACTIVE = 0,
+	SYSEX_STATE_ACTIVE   = 1
+};
+
+#define SYSEX_DUMP {SYSEX_BEGIN,SYSEX_MYID,SYSEX_VERSION,SYSEX_TYPE_DUMP,0,0,0,0,0,{0},{0},SYSEX_END}
+typedef struct {
+	const uint8_t begin;
+	const uint8_t id;
+	const uint8_t version;
+	const uint8_t type;
+	uint8_t uuid;
+	uint8_t state;
+	uint8_t program;
+	uint8_t channel;
+	uint8_t volume;
+	uint8_t program_name[24]; /* Last is always 0 */
+	uint8_t plugin_name[24]; /* Last is always 0 */
+	const uint8_t end;
+} SysExDumpV1;
+
+static void
+sysex_makeASCII(uint8_t* ascii_midi_dest, char* name, size_t size_dest) {
+	size_t i;
+	for (i=0; i < strlen(name) && i < size_dest - 1; i++) {
+		if ( ! isprint( toascii( name[i]) ) )
+			continue;
+
+		ascii_midi_dest[i] = name[i];
+	}
+
+	/* Set rest to 0 */
+	memset(ascii_midi_dest + i, 0, size_dest - i - 1);
+}
+
+#endif /* __sysex_h__ */
